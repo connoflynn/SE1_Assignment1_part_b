3c9178980674108952cff178cb6e249fe400723b
Conn O'Flynn <connoflynn@gmail.com>
2020-11-22 18:13:47 +0000

    First commit

---
+++ src/main/java/com/mycompany/se3_assignment1_part_b/Driver.java
@@ -0,0 +1,17 @@
+package com.mycompany.se3_assignment1_part_b;
+
+import com.mycompany.se3_assignment1.Student;
+import com.mycompany.se3_assignment1.Course;
+import com.mycompany.se3_assignment1.Module;
+
+/**
+ *
+ * @author Conn O'Flynn
+ */
+public class Driver {
+    
+    public static void main(String[] args){
+        Student student = new Student("Conn", 24, "24/06/1996", 15445178);
+        
+    }
+}
